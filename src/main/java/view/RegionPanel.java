24549136f18171f2c0d86a4942882a4b49c986c6
eabneka <115128033+eabneka@users.noreply.github.com>
2022-11-06 17:15:15 +0900

    Create RegionPanel.java

---
+++ src/main/java/view/RegionPanel.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class RegionPanel extends JPanel {
+
+    public RegionPanel() {
+        setBackground(Color.BLUE);
+        add(new JButton("버튼2"));
+        setVisible(true);
+
+    }
+
+}
