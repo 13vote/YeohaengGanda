dc585051e011a8dc8c7606e8cea8fa374e899d68
Seo, Myoungjin <sih5673@gmail.com>
2022-11-14 15:43:12 +0900

    feat Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import controller.MainController;
+import model.Model3;
+import view.AppView;
+import view.MainPanel;
+import view.RegionPanel;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MainController m = new MainController(new AppView());
+
+    }
+}
