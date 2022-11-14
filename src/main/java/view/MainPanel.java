b60f56e1015d44976bfe028dfbbda1a5f78bbf92
Seo, Myoungjin <sih5673@gmail.com>
2022-11-14 14:37:26 +0900

    feat Menubar & DetailPanel test

---
+++ src/main/java/view/MainPanel.java
@@ -0,0 +1,19 @@
+package view;
+
+import controller.MainController;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MainPanel extends JPanel {
+    public JButton btn = new JButton("버튼1");
+    private static MainController mainController;
+    public MainPanel() {
+        setBackground(Color.BLACK);
+        add(btn);
+        setVisible(true);
+    }
+
+
+}
