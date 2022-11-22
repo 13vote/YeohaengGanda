3ed7888e77d5102bc96c1627ead93ecbd00762cc
Crayon <lsh328328@naver.com>
2022-11-22 12:09:45 +0900

    [Feat]: 이미지 경로 처리

--- src/main/java/view/MainPanel.java
+++ src/main/java/view/MainPanel.java
@@ -10,7 +10,7 @@ public class MainPanel extends JPanel {
     public JButton btn = new JButton("MainPanel");
     public MainPanel() {
         setBackground(new Color(99, 195, 249));
-        JLabel label = new JLabel(new ImageIcon("resource/map.png"));
+        JLabel label = new JLabel(new ImageIcon("src/main/java/images/map.png"));
         add(label);
         add(btn);
         setVisible(true);
