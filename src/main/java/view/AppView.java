733a8b8d306c86bc9591555821138c75d99b5aa7
Seo, Myoungjin <sih5673@gmail.com>
2022-11-22 18:56:12 +0900

    feat AppView

--- src/main/java/view/AppView.java
+++ src/main/java/view/AppView.java
@@ -14,7 +14,7 @@ public class AppView extends JFrame{
     private static MainController mainController;
     private String[] name = {"경기도", "경상북도","경상남도","전라북도","전라남도"
             ,"충청북도","충청남도","강원도"};
-    Model3 mm = new Model3(name);
+
 
     public AppView() {
         //main
@@ -22,35 +22,13 @@ public class AppView extends JFrame{
         setSize(600,900);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
-
-        JMenuBar mb = new JMenuBar();
-        mb.setLayout(null);
-        mb.add(new JMenu("메뉴"));
-        mb.add(new JMenu("도움말"));
-
-
-        JComboBox screenMenu = new JComboBox(name);
-
-        mb.setSize(600,40);
-        mb.add(screenMenu).setBounds(496,0,70,30);
-        setJMenuBar(mb);
-
-        add(detailPanel).setBounds(40, 36, 520, 660);
-
+        add(detailPanel);
 
         setVisible(true);
-        btn();
 
     }
 
-    public void btn(){
-        // JButton("여행지 저장하기") 생성
-        JButton btn = new JButton("여행지 저장하기");
-        btn.setBounds(132,721,335,56);
-        add(btn).setBackground(new Color(99,195,249));;
-        setVisible(true);
 
-    }
 
     public void change(JPanel panelName) {
         getContentPane().removeAll();
@@ -59,9 +37,6 @@ public class AppView extends JFrame{
         repaint();
     }
 
-    public static void main(String[] args) {
-        new AppView();
-    }
 
 
 }
