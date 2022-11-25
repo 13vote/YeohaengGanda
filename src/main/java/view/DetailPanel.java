a9fbd9dfeecad9cfa0b294e48574bfc061854af4
eab <rycbar123@office.skhu.ac.kr>
2022-11-25 22:06:21 +0900

    DetailPanel(String[] content, ImageIcon[] images)

--- src/main/java/view/DetailPanel.java
+++ src/main/java/view/DetailPanel.java
@@ -43,6 +43,32 @@ public class DetailPanel extends JPanel {
 
 
     }
+    public DetailPanel(String[] content, ImageIcon[] images) {
+    	this.content = content;
+    	this.images = images;
+    	
+    	// 패널 c
+
+        JPanel f1 = new JPanel();
+        f1.setBounds(40, 36, 520, 660);
+        setLayout(null);
+
+        Color c1 = new Color(99,195,249);
+        f1.setBackground(c1);
+        f1.setLayout(null);
+        f1.add(new JLabel("여행지 이름")).setBounds(18,24,120,30);
+        JLabel j1 = new JLabel("여행지 설명 : "+content[0]);
+        j1.setVerticalAlignment(JLabel.TOP);
+        f1.add(j1).setBounds(18,88,491,391);
+        this.setSize(600,900);
+        add(f1);
+        for(int i=0; i<images.length; i++) {
+            f1.add(new JLabel(images[i])).setBounds(18+150*i,528,120,120);
+        }
+
+        btn();
+        setVisible(true);
+    }
     private void btn(){
         // JButton("여행지 저장하기") 생성
         JButton btn = new JButton("여행지 저장하기");
